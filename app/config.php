120dfc547ee64a7a625097a438ef0cfab963601b
REIMAN\intern02 <gumizinho8@gmail.com>
2023-09-06 15:44:29 +0100

    Added database

--- app/config.php
+++ app/config.php
@@ -3,23 +3,23 @@
 // App Name
 define('APP_NAME','Basic Name Gathering');
 
-// Database Config
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_DATABASE',  'db_bng');
-define('MYSQL_USERNAME',  'user_db_bng');
-define('MYSQL_PASSWORD' ,  'z^L~{oyXDxyN');
+// // Database Config
+// define('MYSQL_HOST', 'localhost');
+// define('MYSQL_DATABASE',  'db_bng');
+// define('MYSQL_USERNAME',  'user_db_bng');
+// define('MYSQL_PASSWORD' ,  'z^L~{oyXDxyN');
 
-// MYSQL encryption key
-define('MYSQL_AES_KEY',     'Vduu47qL51hLn6bkYkY6NlO1nivsmdfD');
+// // MYSQL encryption key
+// define('MYSQL_AES_KEY',     'Vduu47qL51hLn6bkYkY6NlO1nivsmdfD');
 
-// Log path
-define('LOGS_PATH', __DIR__ . '/../logs/app.log');
-
-
-// OPENSLL encryption
-define('OPENSSL_KEY','UAT3cspKczVeVk53OhW0wdRzB5eBlRDA');
-define('OPENSSL_IV','vXoVO5DXuluJ1Lee');
+// // Log path
+// define('LOGS_PATH', __DIR__ . '/../logs/app.log');
 
 
-// Emaoil
-define('EMAIL_FROM' , 'general@bng.com');
+// // OPENSLL encryption
+// define('OPENSSL_KEY','UAT3cspKczVeVk53OhW0wdRzB5eBlRDA');
+// define('OPENSSL_IV','vXoVO5DXuluJ1Lee');
+
+
+// // Email
+// define('EMAIL_FROM' , 'general@bng.com');
